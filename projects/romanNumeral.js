473124123a133787fe3436dd9a0fb09fe680a4fe
Jose Manuel <j.ochoa317@gmail.com>
2023-01-10 17:19:30 -0500

    UI - Caesars Cipher

--- projects/romanNumeral.js
+++ projects/romanNumeral.js
@@ -1,29 +1,40 @@
 const inputNumber=document.querySelector(".input-natural-number");
 const btnConvert=document.querySelector(".convert");
-const resultConvert=document.querySelector(".output-container");
+const resultConvert=document.querySelector(".ouput-container");
 
-btnConvert.addEventListener("click",convertNumber);
+btnConvert.addEventListener("click",validatorFn);
+
+function validatorFn(){
+    if(inputNumber.value>0 && inputNumber.value<=4000){
+        let ouputResult=convertNumber(Number(inputNumber.value));
+        document.querySelector(".number-entered").innerHTML=inputNumber.value;
+        document.querySelector(".number-converted").innerHTML=ouputResult;
+        resultConvert.classList.remove("inactive");
+    }
+}
 
 function convertNumber(num){
     if(num===4000){
+        document.querySelector(".number-converted").classList.add("overline");
         return 'IV'
     }
     const numers=[1000,900,500,400,100,90,50,40,10,9,5,4,1];
     const leters=["M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"];
-    let valor,posicion;
+    let value,position;
     let numleter=[]
     let resto=1;
     while(resto>0){
         for(let i=0;i<numers.length;i++){
             if(num>=numers[i]){
-            valor=numers[i];
-            posicion=i;
-            break;
+                value=numers[i];
+                position=i;
+                break;
             }
         }
-        numleter.push(leters[posicion].repeat(Math.trunc(num/valor)));
-        resto=num%valor
+        numleter.push(leters[position].repeat(Math.trunc(num/value)));
+        resto=num%value
         num=resto;
     }
+    document.querySelector(".number-converted").classList.remove("overline");
     return numleter.join("");
 }
