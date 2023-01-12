19185a8a0271acca1bec521268d12402fef642c1
Jose Manuel <j.ochoa317@gmail.com>
2023-01-12 17:50:11 -0500

    UI - Caesars Cipher II

---
+++ projects/caesarsCipher.js
@@ -0,0 +1,9 @@
+const inputText=document.getElementById("input-text-cipher");
+const btnEncode=document.querySelector(".btn-encode");
+const btnDecode=document.querySelector(".btn-decode");
+
+btnEncode.addEventListener("click",encodeText);
+
+function encodeText(){
+    
+}
